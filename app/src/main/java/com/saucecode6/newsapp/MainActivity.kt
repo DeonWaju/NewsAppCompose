51673abb447452cd7c50bc95ad375e4c9b887664
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-12-01 15:49:00 +0100

    completed

--- app/src/main/java/com/saucecode6/newsapp/MainActivity.kt
+++ app/src/main/java/com/saucecode6/newsapp/MainActivity.kt
@@ -10,9 +10,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
 import com.saucecode6.newsapp.domain.usecases.AppEntryUsecases
 import com.saucecode6.newsapp.presentation.onboarding.OnboardingScreen
+import com.saucecode6.newsapp.presentation.onboarding.OnboardingViewmodel
 import com.saucecode6.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -35,7 +37,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             NewsAppTheme {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
-                    OnboardingScreen()
+                    val viewModel: OnboardingViewmodel = hiltViewModel()
+                    OnboardingScreen(
+                        event = viewModel::onEvent
+                    )
                 }
             }
         }
