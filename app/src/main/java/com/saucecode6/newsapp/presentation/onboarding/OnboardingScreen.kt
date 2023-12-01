2505fd6774d3b35e3e529fa30f4e6a4ed32e9a7b
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-12-01 01:27:52 +0100

    created news button, pageIndicator

--- app/src/main/java/com/saucecode6/newsapp/presentation/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/saucecode6/newsapp/presentation/onboarding/OnboardingScreen.kt
@@ -1,8 +1,14 @@
 package com.saucecode6.newsapp.presentation.onboarding
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun OnboardingScreen(){
+    Column(modifier = Modifier.fillMaxSize()) {
+
+    }
 
 }
