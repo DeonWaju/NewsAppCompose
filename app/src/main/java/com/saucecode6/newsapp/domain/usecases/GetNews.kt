3c4256462506fe9f4ef1ccf3218ab1e7f3453b73
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-12-03 13:44:29 +0100

    domain for news sources

---
+++ app/src/main/java/com/saucecode6/newsapp/domain/usecases/GetNews.kt
@@ -0,0 +1,15 @@
+package com.saucecode6.newsapp.domain.usecases
+
+import androidx.paging.PagingData
+import com.saucecode6.newsapp.domain.model.Article
+import com.saucecode6.newsapp.domain.repo.INewsRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetNews(
+    private val iNewsRepository: INewsRepository
+) {
+
+    operator fun invoke(sources: List<String>): Flow<PagingData<Article>>{
+        return iNewsRepository.getNews(sources = sources)
+    }
+}
