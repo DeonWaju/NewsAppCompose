50ee3a9142f2a11c6bf7f6fcc4dbca07a44cd61d
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-12-03 22:53:27 +0100

    completed HomeScreen

--- app/src/main/java/com/saucecode6/newsapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/saucecode6/newsapp/presentation/home/HomeScreen.kt
@@ -1,6 +1,8 @@
 package com.saucecode6.newsapp.presentation.home
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,20 +11,25 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.paging.compose.LazyPagingItems
 import com.saucecode6.newsapp.R
 import com.saucecode6.newsapp.domain.model.Article
+import com.saucecode6.newsapp.presentation.common.ArticlesList
 import com.saucecode6.newsapp.presentation.common.SearchBar
 import com.saucecode6.newsapp.presentation.navGraph.Route
 import com.saucecode6.newsapp.presentation.onboarding.Dimens.MediumPadding1
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(articles: LazyPagingItems<Article>, navigate: (String) -> Unit){
     val titles by remember{
@@ -63,6 +70,23 @@ fun HomeScreen(articles: LazyPagingItems<Article>, navigate: (String) -> Unit){
             onSearch = {},
             onClick = {
                 navigate(Route.SearchScreen.route)
+            }
+        )
+        Spacer(modifier = Modifier.height(MediumPadding1))
+        Text(
+            text = titles, modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = MediumPadding1)
+                .basicMarquee(), fontSize = 12.sp,
+            color = colorResource(id = R.color.placeholder)
+        )
+
+        Spacer(modifier = Modifier.height(MediumPadding1))
+        ArticlesList(
+            modifier = Modifier.padding(horizontal = MediumPadding1),
+            articles = articles,
+            onClick = {
+                //TODO: Navigate to Details Screen
             }
         )
     }
