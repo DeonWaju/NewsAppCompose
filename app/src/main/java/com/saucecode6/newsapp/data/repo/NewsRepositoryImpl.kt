276bf9d7fed0fcace80082fa3ea947dcd58d3963
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-12-04 11:38:16 +0100

    worked on search events and SearchNewsPagingSource.kt

--- app/src/main/java/com/saucecode6/newsapp/data/repo/NewsRepositoryImpl.kt
+++ app/src/main/java/com/saucecode6/newsapp/data/repo/NewsRepositoryImpl.kt
@@ -4,6 +4,7 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.saucecode6.newsapp.data.remote.NewsPagingSource
+import com.saucecode6.newsapp.data.remote.SearchNewsPagingSource
 import com.saucecode6.newsapp.data.remote.api.NewsApi
 import com.saucecode6.newsapp.domain.model.Article
 import com.saucecode6.newsapp.domain.repo.INewsRepository
@@ -11,13 +12,26 @@ import kotlinx.coroutines.flow.Flow
 
 class NewsRepositoryImpl(
     private val newsApi: NewsApi
-): INewsRepository {
+) : INewsRepository {
 
     override fun getNews(sources: List<String>): Flow<PagingData<Article>> {
         return Pager(
             config = PagingConfig(pageSize = 10),
             pagingSourceFactory = {
                 NewsPagingSource(
+                    newsApi = newsApi,
+                    sources = sources.joinToString(separator = ",")
+                )
+            }
+        ).flow
+    }
+
+    override fun searchNews(searchQuery: String, sources: List<String>): Flow<PagingData<Article>> {
+        return Pager(
+            config = PagingConfig(pageSize = 10),
+            pagingSourceFactory = {
+                SearchNewsPagingSource(
+                    searchQuery = searchQuery,
                     newsApi = newsApi,
                     sources = sources.joinToString(separator = ",")
                 )
--- app/src/main/java/com/saucecode6/newsapp/domain/repo/INewsRepository.kt
+++ app/src/main/java/com/saucecode6/newsapp/domain/repo/INewsRepository.kt
@@ -7,4 +7,5 @@ import kotlinx.coroutines.flow.Flow
 interface INewsRepository {
 
     fun getNews(sources: List<String>): Flow<PagingData<Article>>
+    fun searchNews(searchQuery: String, sources: List<String>): Flow<PagingData<Article>>
 }
