de4bcda9b955594eaf5569c334a3df499e457ed4
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-12-05 22:35:46 +0100

    round ups

--- app/src/main/java/com/saucecode6/newsapp/presentation/news_navigator/components/NewsNavigator.kt
+++ app/src/main/java/com/saucecode6/newsapp/presentation/news_navigator/components/NewsNavigator.kt
@@ -133,7 +133,7 @@ fun NewsNavigator() {
             composable(route = Route.DetailsScreen.route) {
                 val viewModel: DetailsViewModel = hiltViewModel()
                 if (viewModel.sideEffect != null) {
-                    Toast.makeText(LocalContext.current, "Saved", Toast.LENGTH_LONG)
+                    Toast.makeText(LocalContext.current, viewModel.sideEffect, Toast.LENGTH_SHORT).show()
                     viewModel.onEvent(DetailsEvent.RemoveSideEffect)
                 }
                 navController.previousBackStackEntry?.savedStateHandle?.get<Article?>("article")
