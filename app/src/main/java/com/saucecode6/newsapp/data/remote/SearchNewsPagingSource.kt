962b0ec51b8a682014c56e0944d360bde0c4784e
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-12-05 23:31:53 +0100

    fixed key issues in paging library causing app crash

--- app/src/main/java/com/saucecode6/newsapp/data/remote/SearchNewsPagingSource.kt
+++ app/src/main/java/com/saucecode6/newsapp/data/remote/SearchNewsPagingSource.kt
@@ -9,7 +9,7 @@ class SearchNewsPagingSource(
     private val newsApi: NewsApi,
     private val searchQuery: String,
     private val sources: String
-): PagingSource<Int, Article>() {
+) : PagingSource<Int, Article>() {
 
     private var totalNewsCount = 0
 
@@ -18,24 +18,27 @@ class SearchNewsPagingSource(
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
         val page = params.key ?: 1
         return try {
-            val newsResponse = newsApi.searchNews(searchQuery = searchQuery, sources = sources, page = page)
+            val newsResponse =
+                newsApi.searchNews(searchQuery = searchQuery, sources = sources, page = page)
             totalNewsCount += newsResponse.articles.size
             val articles = newsResponse.articles.distinctBy { it.title }
             LoadResult.Page(
                 data = articles,
-                nextKey = if(totalNewsCount == newsResponse.totalResults) null else + 1,
+                nextKey = if (totalNewsCount == newsResponse.totalResults) null else +1,
                 prevKey = null
             )
-        } catch (e: Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
             LoadResult.Error(
                 throwable = e
             )
-        }    }
+        }
+    }
 
     override fun getRefreshKey(state: PagingState<Int, Article>): Int? {
-        return state.anchorPosition?.let {anchorPosition ->
+        return state.anchorPosition?.let { anchorPosition ->
             val anchorPage = state.closestPageToPosition(anchorPosition)
-            anchorPage?.prevKey?.plus(1)?: anchorPage?.nextKey?.minus(1)
-        }    }
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
 }
